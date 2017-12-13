e76a590384fd759e1bb08603241c06c9009694be
Kevin Deerajviset <kevin.deerajviset@gmail.com>
2017-12-13 12:52:28 +1100

    made index look better

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     app = express();
     methodOverride = require("method-override");
+    expressSanitizer = require("express-sanitizer");
 
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE MODEL CONFIG
@@ -53,6 +55,7 @@ app.get("/blogs/new", function(req, res) {
 // CREATE ROUTE
 app.post("/blogs", function(req, res) {
     // create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
        if(err) {
           res.render("new");
@@ -87,6 +90,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
       if(err) {
         res.redirect("/blogs");
